27f978ae9428b5f9dc789ec888eb23ec29e5951a
thirao <hirao.tatsuya@gmail.com>
2016-12-12 17:00:09 +0900

    second

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class MainHandler(tornado.web.RequestHandler):
         self.render(
             "main.html",
             page_title='Heroku Funtimes',
-            page_heading='Hi!',
+            page_heading='fuga!',
             google_analytics_id=google_analytics_id,
         )
 
